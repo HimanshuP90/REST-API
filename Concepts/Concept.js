355cebc8d014ac1fffe65db05db7801ae58bde8b
HimanshuP90 <himanshup895@gmail.com>
2018-02-08 12:16:26 +0530

    Added basic setup & express

---
+++ Concepts/Concept.js
@@ -0,0 +1,23 @@
+What is a REST API ?
+API: Application Programming Interface.
+REST: REpresentational State Transfer.
+
+
+REST: 
+	1) Architectual style of the web.
+	2) A standard/ set of guidelines by which we can structure and create API's.
+
+REST Properties:
+
+@ They make use of HTTP Methods
+	GET: Used to retreive data from the server
+	POST: Used to send data to the server
+	PUT: Used to update data
+	DELETE: Used to delete data
+
+@ They make use of HTTP Status code
+	200 OK 
+	404 resoure not found
+	500 server error
+
+Todo: Create an API using Node.js, Express & MongoDB 
