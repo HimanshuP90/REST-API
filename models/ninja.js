59dd25b60c8a401462cd57e001e6793a21b05f0d
HimanshuP90 <himanshup895@gmail.com>
2018-02-11 14:49:24 +0530

    Frontend add with api call

--- models/ninja.js
+++ models/ninja.js
@@ -1,21 +1,32 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-//create ninja Schema & model
-const NinjaSchema = new Schema({
-	name: {
-		type: String,
-		required: [true, 'Name field is reqquired']
-	},
-	
-	rank: {
-		type: String
-	},
+// create geolocation Schema
+const GeoSchema = new Schema({
+    type: {
+        type: String,
+        default: 'Point'
+    },
+    coordinates: {
+        type: [Number],
+        index: '2dsphere'
+    }
+});
 
-	available: {
-		type: Boolean,
-		dafault: false
-	}
+// create ninja Schema & model
+const NinjaSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Name field is required']
+    },
+    rank: {
+        type: String
+    },
+    available: {
+        type: Boolean,
+        default: false
+    },
+    geometry: GeoSchema
 });
 
 const Ninja = mongoose.model('ninja', NinjaSchema);
